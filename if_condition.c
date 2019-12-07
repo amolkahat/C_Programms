19e9b1fdbddce245fb13204f89d214e49dad36bf
Amol Kahat <amolkahat@gmail.com>
2019-12-07 15:15:43 +0530

    Minor fixes
    Signed-off-by: Amol Kahat <amolkahat@gmail.com>

--- if_condition.c
+++ if_condition.c
@@ -2,16 +2,16 @@
 void main()
 {
  int a, b, c;
-    printf("Enter 1st Number\n");
-    scanf("%d",&a);
-    printf("Enter 2nd Number\n");
-    scanf("%d"&b);
+    printf("Enter 1st Number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd Number: ");
+    scanf("%d", &b);
     if (a > b){
-        printf("A is greater");
+        printf("A is greater.\n");
     }
     else if(b > a){
-        {printf(" B is grater");
-        }
+	 printf("B is greater. \n");
+    }
     else{
         printf("ERROR: Unable to compare.\n");
     }
