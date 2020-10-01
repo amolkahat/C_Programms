b366aa9517f28bfd1386489eb43888398f7093b0
shubh140601 <45026874+shubh140601@users.noreply.github.com>
2020-10-01 08:33:12 +0530

    Update if_condition.c

--- if_condition.c
+++ if_condition.c
@@ -2,10 +2,9 @@
 void main()
 {
  int a, b, c;
-    printf("Enter 1st Number: ");
-    scanf("%d", &a);
-    printf("Enter 2nd Number: ");
-    scanf("%d", &b);
+    printf("Enter 1st and 2nd Number: ");
+    scanf("%d"%d, &a,&b);
+    
     if (a > b){
         printf("A is greater.\n");
     }
